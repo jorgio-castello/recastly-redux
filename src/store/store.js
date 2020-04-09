aff28679e73bb54e1166c55ed7ad40e671d06391
Jorge Castello <jorgio.castello@gmail.com>
2020-04-09 10:08:44 -0700

    Implement VideoListContainer

--- src/store/store.js
+++ src/store/store.js
@@ -3,8 +3,14 @@ import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
+let initialState = {
+  videos: [],
+  video: {}
+};
+
+
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
-let store = createStore(rootReducer, applyMiddleware(thunk));
+let store = createStore(rootReducer, initialState, applyMiddleware(thunk));
 
 //Dispatch the example video data
 store.dispatch({
@@ -13,3 +19,26 @@ store.dispatch({
 });
 
 export default store;
+
+/*
+  initialState = {
+    videoList: [],
+    currentVideo: {}
+  }
+
+
+  videoListReducer
+    //We accept state we are accepting the whole object
+    case CHANGE_VIDEO_LIST:
+      return {
+        ...state,
+        videoList: action.videos
+      }
+
+  videoReducer
+      case CHANGE_CURRENT_VIDEO:
+        return {
+          ...state,
+          currentVideo: action.video
+        }
+*/
