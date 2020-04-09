9cd65c1b188c6206f6b2cd72a216917a1f367fff
Jorge Castello <jorgio.castello@gmail.com>
2020-04-09 12:12:27 -0700

    Save current state of work

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
-import searchYouTube from './lib/searchYouTube.js';
-import YOUTUBE_API_KEY from './config/youtube.js';
+// import searchYouTube from './lib/searchYouTube.js';
+// import YOUTUBE_API_KEY from './config/youtube.js';
+import store from './store/store.js';
+import handleVideoSearch from './actions/search.js';
+import { Provider } from 'react-redux';
 
 //TODO: Import the Provider component from 'react-redux' here!
 
@@ -10,6 +13,8 @@ import YOUTUBE_API_KEY from './config/youtube.js';
 //  the rest of your app.
 
 ReactDOM.render(
-  <App API_KEY={YOUTUBE_API_KEY} searchYouTube={searchYouTube} />,
+  <Provider store = {store} >
+    <App searchYouTube={handleVideoSearch} />
+  </Provider>,
   document.getElementById('app')
 );
